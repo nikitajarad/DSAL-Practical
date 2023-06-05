cec44e82407cb636eec3dfca68cdf03fc41cdd9a
nikitajarad <86143748+nikitajarad@users.noreply.github.com>
2023-06-05 22:17:28 +0530

    Add files via upload

--- Prac 4_Exp Tree.cpp
+++ Prac 4_Exp Tree.cpp
@@ -2,125 +2,143 @@
 #include <stack>
 #include <string>
 using namespace std;
- 
- class node
+
+class node
 {
-	public:
-	node *right;
-	node *left;
-	node *root;
-	char data;
-	
-	node(char data,node* left, node* right)
-	{
-	this->data = data;
+public:
+    node *right;
+    node *left;
+    node *root;
+    char data;
+
+    node(char data, node *left, node *right)
+    {
+        this->data = data;
         this->left = left;
         this->right = right;
-	}
-	node(char data)
-	{
-	  this->data = data;
-	}
-	
-	node()
-	{
-		root=NULL;
-	}
+    }
+    node(char data)
+    {
+        this->data = data;
+    }
+
+    node()
+    {
+        root = NULL;
+    }
 };
 
-bool isOperator(char c) 
+bool isOperator(char c)
 {
     return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
 }
 
 void postorder(node *root)
 {
-    if(root!=NULL)
-    {
-        
-        postorder(root->left);
-        postorder(root->right);
-        cout<<" "<<root->data;      
-    }
-}
-
-
-
-void inorder(node* root)
-{
-    if (root == NULL) 
+    if (root == NULL)
     {
         return;
     }
 
-    if (isOperator(root->data)) 
+    stack<node *> s;
+    node *current = root;
+    node *prev = NULL;
+
+    while (current || !s.empty())
+    {
+        if (current)
+        {
+            s.push(current);
+            current = current->left;
+        }
+        else
+        {
+            node *temp = s.top();
+
+            // If the right child exists and traversing
+            // from the left child, then move right
+            if (temp->right && temp->right != prev)
+            {
+                current = temp->right;
+            }
+            else
+            {
+                cout << temp->data << " ";
+                s.pop();
+                prev = temp;
+            }
+        }
+    }
+}
+
+void inorder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    if (isOperator(root->data))
     {
         cout << "(";
     }
- 
+
     inorder(root->left);
     cout << root->data;
     inorder(root->right);
- 
- 
-    if (isOperator(root->data)) 
+
+    if (isOperator(root->data))
     {
         cout << ")";
     }
 }
 
-
-node* exptree(string postfix)
+node *exptree(string postfix)
 {
-    if (postfix.length() == 0) 
+    if (postfix.length() == 0)
     {
         return NULL;
     }
- 
-    // create an empty stack to store tree pointers
-    stack<node*> s;
- 
-    // traverse the postfix exp
-    for (char c: postfix)
+
+    stack<node *> s;
+
+    for (char c : postfix)
     {
         if (isOperator(c))
         {
-        
-            node* x = s.top();
+            node *x = s.top();
             s.pop();
- 
-            node* y = s.top();
+
+            node *y = s.top();
             s.pop();
- 
-            node* nod = new node(c, y, x);
- 
+
+            node *nod = new node(c, y, x);
+
             s.push(nod);
         }
- 
-        else 
+        else
         {
             s.push(new node(c));
         }
     }
- 
+
     return s.top();
 }
- 
+
 int main()
 {
-    //string postfix = "ab+cde+**";
-    string postfix;
-    cout<<"Enter the expression:";
-    cin>>postfix;
-    node* root = exptree(postfix);
- 
+       //string postfix = "ab+cde+**";
+     string postfix;
+    cout << "Enter the expression:";
+    cin >> postfix;
+    node *root = exptree(postfix);
+
     cout << "Postfix Expression: ";
     postorder(root);
- 
+
     cout << "\nInfix Expression: ";
     inorder(root);
-    cout<<"\n";
- 
+    cout << "\n";
+
     return 0;
 }
-
